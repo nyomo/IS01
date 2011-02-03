2c566cac25d6e3ba43b56446acf1a9d5af321298
nyomo <nyomoko+git@gmail.com>
2011-02-03 21:00:17 +0900

    KERNEL_IMG環境変数にブートイメージに組み込みたいkernelを指定出来る様にした

--- dir2img.sh
+++ dir2img.sh
@@ -7,9 +7,12 @@ BOOTFS=bootfs_tmp.img
 IMGDIR=./img
 UBICONF=ubi.cfg
 MKBOOTIMG_OPT="console=ttyMSM2,115200n8 androidboot.hardware=qcom"
-KERNEL="${IMGDIR}/$BASEDIR.out-kernel"
-
+KERNEL_IMG=$(echo $KERNEL_IMG)
 BASEDIR=$*
+[ -z $KERNEL_IMG ] && KERNEL="${IMGDIR}/$BASEDIR.out-kernel"
+[ ! -z $KERNEL_IMG ] && KERNEL="${IMGDIR}/$KERNEL_IMG"
+
+
 [ ! -d $IMGDIR ] && echo "error: do mkdir ./img" && exit
 [ ! -f $UBICONF ]&& echo "error: UBICONF($UBICONF) notfound" && exit
 [ ! -d $BASEDIR ]&& echo "usage: dir2img.sh targetdir"&&exit
@@ -17,6 +20,7 @@ BASEDIR=$*
 
 NEWFILE=${IMGDIR}/my_${BASEDIR} 
 $MKBOOTFS $BASEDIR > ${IMGDIR}/${BOOTFS} && \
+echo "mkbootimg with $KERNEL"
 $MKBOOTIMG --kernel $KERNEL --ramdisk ${IMGDIR}/$BOOTFS --cmdline "'$MKBOOTIMG_OPT'" --base 0x20000000 -o $NEWFILE \
 && echo "create $NEWFILE" 
 

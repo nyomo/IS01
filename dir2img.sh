e93cf0456f7b3874d5f907ee56ad71979027fee8
nyomo <nyomoko+git@gmail.com>
2011-02-04 08:59:38 +0900

    動作チェックして動くイメージが出来る所まで修正した

--- dir2img.sh
+++ dir2img.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-UNUBINIZE=unubinize.pl
-SPLIT_BOOTIMAGE=split_bootimg.pl
-MKBOOTIMG=mkbootimg
-MKBOOTFS=mkbootfs
-BOOTFS=bootfs_tmp.img
+MKBOOTIMG=~/bin/mkbootimg
+MKBOOTFS=~/bin/mkbootfs
+BOOTFS=tmp_bootfs.img
+BOOTIMG=tmp_bootimg.img
 IMGDIR=./img
 UBICONF=ubi.cfg
 MKBOOTIMG_OPT="console=ttyMSM2,115200n8 androidboot.hardware=qcom"
@@ -12,15 +11,20 @@ BASEDIR=$*
 [ -z $KERNEL_IMG ] && KERNEL="${IMGDIR}/$BASEDIR.out-kernel"
 [ ! -z $KERNEL_IMG ] && KERNEL="${IMGDIR}/$KERNEL_IMG"
 
-
 [ ! -d $IMGDIR ] && echo "error: do mkdir ./img" && exit
 [ ! -f $UBICONF ]&& echo "error: UBICONF($UBICONF) notfound" && exit
 [ ! -d $BASEDIR ]&& echo "usage: dir2img.sh targetdir"&&exit
 [ ! -f $KERNEL ]&& echo "error: kernel($KERNEL) notfound" && exit
 
 NEWFILE=${IMGDIR}/my_${BASEDIR} 
-$MKBOOTFS $BASEDIR > ${IMGDIR}/${BOOTFS} && \
-echo "mkbootimg with $KERNEL"
-$MKBOOTIMG --kernel $KERNEL --ramdisk ${IMGDIR}/$BOOTFS --cmdline "'$MKBOOTIMG_OPT'" --base 0x20000000 -o $NEWFILE \
-&& echo "create $NEWFILE" 
+rm -r ramdisk
+cp -a $BASEDIR ramdisk
+echo "make ramdisk image with $KERNEL"
+$MKBOOTFS ramdisk > $IMGDIR/$BOOTFS
+
+$MKBOOTIMG --kernel $KERNEL --ramdisk $IMGDIR/$BOOTFS --cmdline "$MKBOOTIMG_OPT" --base 0x20000000 -o $IMGDIR/$BOOTIMG
+
+echo "create $NEWFILE" 
+rm -f $NEWFILE
+ubinize -o $NEWFILE -p 128KiB -m 2048 -O 256 $UBICONF
 
